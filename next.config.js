a0c1d7c8e548df841abbb53a66a99264e0572738
Brett Ball <madswitchman@yahoo.com>
2026-02-19 21:50:28 -0600

    fix: add basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/pudding-website',
+  assetPrefix: '/pudding-website/',
   images: {
     unoptimized: true,
   },
