8945d8540701370d95bdd20f4fe9d71ef506b91b
Brett Ball <madswitchman@yahoo.com>
2026-02-19 21:58:40 -0600

    remove basePath for custom domain

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/pudding-website',
-  assetPrefix: '/pudding-website/',
   images: {
     unoptimized: true,
   },
